7fd93219e535bd6220804a421a6e68e4eddae386
mayajaral <35385165+mayajaral@users.noreply.github.com>
2020-09-30 22:54:11 +0530

    Adding PyPoll.py file and resource files

---
+++ PyPoll.py
@@ -0,0 +1,44 @@
+# Import the datetime class from the datetime module.
+import datetime as dt
+# Use the now() attribute on the datetime class to get the present time.
+now = dt.datetime.now()
+# Print the present time.
+print("The time right now is ", now)
+
+# Add our dependencies.
+import csv
+import os
+# Assign a variable to load a file from a path.
+file_to_load = os.path.join("Resources", "election_results.csv")
+# Assign a variable to save the file to a path.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+# Open the election results and read the file.
+with open(file_to_load) as election_data:
+    file_reader = csv.reader(election_data)
+
+    # Read and print the header row.
+    headers = next(file_reader)
+    print(headers)
+
+
+
+
+#Write down the names of all the candidates.
+#Add a vote count for each candidate.
+#Get the total votes for each candidate.
+#Get the total votes cast for the election.
+
+
+    
+
+# Create a filename variable to a direct or indirect path to the file.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+# Using the with statement open the file as a text file.
+with open(file_to_save, "w") as txt_file:
+
+    # Write some data to the file.
+    txt_file.write("Arapahoe\nDenver\nJefferson")
+
+
