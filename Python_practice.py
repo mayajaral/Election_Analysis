8e446eddd6b1776ec64bf525c514a9a839ed807b
mayajaral <35385165+mayajaral@users.noreply.github.com>
2020-10-01 08:58:31 +0530

    Finished Deliverable 1 and 2

--- Python_practice.py
+++ Python_practice.py
@@ -1,3 +1,27 @@
-counties = ["Arapahoe","Denver","Jefferson"]
-if counties[1] == 'Denver':
-    print(counties[1])
+# name = "Denis"
+# country = "United States"
+# age = 35
+# hourly_wage = 60
+# satisfied = True
+# daily_wage = hourly_wage * 8
+# print(f"Hello {name}")
+# print(f"You live in {country}")
+# print(f"You are {age} years old")
+# print(f"You make {daily_wage}$ per day")
+# print(f"Are you satisfied with your pay: {satisfied}")
+
+
+# grocery_list = ['Milk','Bread','Eggs','Peanut Butter','Jelly']
+# print(grocery_list)
+# grocery_list[3] = 'Almond Butter'
+# print(grocery_list)
+# grocery_list.remove('Jelly')
+# print(grocery_list)
+# grocery_list.append('Coffee')
+# print(grocery_list)
+
+maya = {'name': 'Maya', 'age': 22, 'hobbies': ['swimming', 'dancing', 'reading'], 'wakeup_time':'7:00am'}
+print(f"My name is {maya['name']}.")
+print(f"I am {maya['age']}.")
+print(f"My hobbies are {maya['hobbies'][0]}, {maya['hobbies'][1]} and {maya['hobbies'][2]}.")
+print(f"I wake up at {maya['wakeup_time']} every morning.")
